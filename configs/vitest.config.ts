e856fc2b963eb456de328e7124a56e090d29573b
noshiro-pf <noshiro.pf@gmail.com>
2025-09-14 13:42:46 +0900

    ci: fix codecov settings

--- configs/vitest.config.ts
+++ configs/vitest.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     },
     coverage: {
       provider: 'v8',
-      reporter: ['html', 'lcov', 'text'],
+      reporter: ['html', 'lcov', 'text', 'cobertura'],
       include: ['src/**'],
       exclude: ['**/index.mts'],
     },
